70bc26567dae686525f18a4673d287061f47c553
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-08-06 16:14:43 -0500

    Se crea el menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+  menu: MenuItem[] = [
+    {
+      ruta: 'graphics/bar',
+      texto: 'Barras'
+    },
+    {
+      ruta: 'graphics/double-bar',
+      texto: 'Barras dobles'
+    },
+    {
+      ruta: 'graphics/donut',
+      texto: 'Dona'
+    },
+    {
+      ruta: 'graphics/donut-http',
+      texto: 'Dona Http'
+    },
+  ];
 }
