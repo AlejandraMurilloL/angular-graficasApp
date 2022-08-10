fe9f73b8c07b471ce4652763f36db0ef1340e891
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-08-09 19:32:50 -0500

    Componente de grafica de barras pasandole la data

--- src/app/graphics/components/bar-graph/bar-graph.component.ts
+++ src/app/graphics/components/bar-graph/bar-graph.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-bar-graph',
@@ -6,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BarGraphComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BarGraphComponent  {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @Input() barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: []
+  };
+  
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  
+  public barChartType: ChartType = 'bar';  
 }
--- src/app/graphics/pages/double-bar/double-bar.component.ts
+++ src/app/graphics/pages/double-bar/double-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-double-bar',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DoubleBarComponent implements OnInit {
+export class DoubleBarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  proveedoresData: ChartData<'bar'>  = {
+    labels: ['2021', '2022','2023','2024','2025'],
+    datasets: [
+      { data: [ 100,200,300,400,500 ], label: 'Vendedor A' },
+      { data: [ 50,250,30, 450,200 ], label: 'Vendedor B' },
+    ]
+  };
+  
+  
+  public productoData: ChartData<'bar'>  = {
+    labels: ['2021', '2022','2023','2024','2025'],
+    datasets: [{ data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' },]
+  };
 }
